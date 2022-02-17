a62b583ecd2e78e89a24492845b7cff7279519c1
tejasdesai <tejasdesai0531@gmail.com>
2022-02-17 16:10:01 +0000

    Adds auth service and common package

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { currentUser } from '@tdfoodorder/common';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  console.log(req.currentUser)
+  res.send({ currentUser: req.currentUser || null });
+});
+
+export { router as currentUserRouter };
